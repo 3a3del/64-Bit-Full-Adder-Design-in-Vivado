e67c02bb6a81710f75203d7a8e5e5aa4f4ad16b2
Muhammed Adel <114674400+3a3del@users.noreply.github.com>
2024-08-01 11:52:17 -0700

    Sources

---
+++ sources/bd/Universal/hdl/Universal_wrapper.v
@@ -0,0 +1,20 @@
+//Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+//Date        : Thu Aug  1 10:04:13 2024
+//Host        : DESKTOP-9SQ9M3B running 64-bit major release  (build 9200)
+//Command     : generate_target Universal_wrapper.bd
+//Design      : Universal_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module Universal_wrapper
+   (clk);
+  input clk;
+
+  wire clk;
+
+  Universal Universal_i
+       (.clk(clk));
+endmodule
---
+++ sources/bd/Universal/ip/Universal_c_addsub_0_0/Universal_c_addsub_0_0_stub.v
@@ -0,0 +1,26 @@
+// Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
+// Date        : Thu Aug  1 10:08:00 2024
+// Host        : DESKTOP-9SQ9M3B running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               c:/Users/hp/Universal/Universal.srcs/sources_1/bd/Universal/ip/Universal_c_addsub_0_0/Universal_c_addsub_0_0_stub.v
+// Design      : Universal_c_addsub_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z020clg484-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "c_addsub_v12_0_12,Vivado 2018.2" *)
+module Universal_c_addsub_0_0(A, B, CLK, ADD, C_IN, C_OUT, S)
+/* synthesis syn_black_box black_box_pad_pin="A[63:0],B[63:0],CLK,ADD,C_IN,C_OUT,S[63:0]" */;
+  input [63:0]A;
+  input [63:0]B;
+  input CLK;
+  input ADD;
+  input C_IN;
+  output C_OUT;
+  output [63:0]S;
+endmodule
